46f00ab6853bd475f74f0a2dd588b590c6051f38
Gra <grace.bulckens@gmail.com>
2024-12-02 21:26:53 +0100

    Day 1

---
+++ src/01/01.ts
@@ -0,0 +1,36 @@
+const input: string[] = require("fs")
+  .readFileSync(require("path").join(__dirname, "./input.txt"), "utf-8")
+  .split("\r\n");
+
+export function parse(input: string): [number[], number[]] {
+  const row1: number[] = [];
+  const row2: number[] = [];
+  input.split("\r\n").forEach((line) => {
+    const [a, b] = line.trim().split(/\s+/).map(Number);
+    row1.push(a!);
+    row2.push(b!);
+  });
+  return [row1, row2];
+}
+
+export function partOne([row1, row2]: [number[], number[]]): number {
+  row1.sort((a, b) => a - b);
+  row2.sort((a, b) => a - b);
+  return row1.reduce((sum, a, index) => sum + Math.abs(a - row2[index]!), 0);
+}
+
+export function partTwo([row1, row2]: [number[], number[]]): number {
+  const row2Count = row2.reduce(
+    (acc, num) => {
+      acc[num] = (acc[num] || 0) + 1;
+      return acc;
+    },
+    {} as Record<number, number>,
+  );
+  return row1.reduce((sum, a) => sum + a * (row2Count[a] || 0), 0);
+}
+
+const [row1, row2] = parse(input.join("\r\n"));
+
+partOne([row1, row2]);
+partTwo([row1, row2]);
